dbdc9ac2a6ee6208125429c79482e52beea37de2
Matteo Conti <56634851+matteo-conti-97@users.noreply.github.com>
2023-04-27 10:57:07 +0200

    implemented gatewayApi with rest

--- gatewayApiPack/gatewayApi/LoginClient.py
+++ gatewayApiPack/gatewayApi/LoginClient.py
@@ -1,11 +1,23 @@
 import protos.login_service_cs_pb2
 import protos.login_service_cs_pb2_grpc
 import grpc
+from flask import Flask, json
+
+api = Flask(__name__)
 
 
-if __name__ == "__main__":
+@api.route('/login', methods=['GET'])
+def get_companies():
+    res={}
     channel = grpc.insecure_channel('localhost:50051')
     stub = protos.login_service_cs_pb2_grpc.LoginStub(channel)
     login_request = protos.login_service_cs_pb2.LoginCredentials(username="prova", password="1234")
     response = stub.RpcLogin(login_request)
+    res['message'] = response.message
+    res['token'] = response.token
     print("LoginService client received: " + response.message + " Token: " + response.token)
+    return json.dumps(res)
+
+
+if __name__ == '__main__':
+    api.run()
