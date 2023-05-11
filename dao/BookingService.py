70cbff8ed80c99f49fe7762a0435d89e4b24428c
Stefan Adrian Huma <stefanadrian.huma@alumni.uniroma2.eu>
2023-05-11 13:13:42 +0200

    Creazione dataBase per supporto alle prenotazione e microservizio BookingService

---
+++ dao/BookingService.py
@@ -0,0 +1,52 @@
+from neo4j import GraphDatabase
+
+from connect import create_person, create_stop, create_startStop, create_endStop
+
+
+class Neo4jMicroservice:
+    def __init__(self, uri, auth):
+        self.driver = GraphDatabase.driver(uri, auth=auth)
+        self.driver.verify_connectivity()
+        self.db = "neo4j"
+
+    #Funzioni Dao
+
+    #Aggiungi persona
+    def insert_person(self, name):
+        session = self.driver.session(database=self.db)
+        session.execute_write(create_person, name)
+        session.close()
+
+    def insert_stop(self, name, hour, date):
+        session = self.driver.session(database=self.db)
+        session.execute_write(create_stop, name, hour, date)
+        session.close()
+
+    def insert_startStop(self, user, stop, hour, date):
+        session = self.driver.session(database=self.db)
+        session.execute_write(create_startStop, user, stop, hour, date)
+        session.close()
+
+    def insert_endStop(self, user, stop, hour, date):
+        session = self.driver.session(database=self.db)
+        session.execute_write(create_endStop, user, stop, hour, date)
+        session.close()
+
+    #L'esecuzione è un placeholder quindi alcuni dati non hanno un senso logico
+    def insert_booking(self , user , starting_point , ending_point , data , arrival_time , travel_time):
+            self.insert_startStop(user, starting_point, arrival_time, data)
+            self.insert_endStop(user, ending_point, arrival_time+travel_time, data)
+
+
+
+
+# Esempio di utilizzo
+uri = "bolt://localhost:7687"
+auth=("neo4j", "password")
+service = Neo4jMicroservice(uri, auth)
+
+service.insert_person("Luca")
+service.insert_stop("Tor Vergata (Medicina)", "13:45", "11/05/2023")
+service.insert_startStop("Luca", "Tor Vergata (Medicina)", "13:45", "11/05/2023")
+service.insert_endStop("Luca", "Anagnina", "14:00", "11/05/2023")
+service.insert_booking("Stefan", "Anagnina", "Tor Vergata", "12/05/2023", "14:00", "00:15")
