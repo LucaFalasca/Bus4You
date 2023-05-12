814b1b16a90c8b9576949a60c7d48f83c25a8d66
Luca Falasca <lucafalasca08@gmail.com>
2023-05-12 17:06:49 +0200

    #23 - GatewayAPI and Frontend Refactoring for Improved Scalability and Performance

--- frontend/login/main.py
+++ frontend/login/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, session, flash
 import requests
-from entity.User import User
 
 app = Flask(__name__)
 app.secret_key = '1234 bianchi legge questo e si sente male'
@@ -21,7 +20,7 @@ def login():
         flash('Login failed some required fields are empty')
         return render_template("login.html")
     else:
-        gateway_login_url = 'http://localhost:50052/login?usr=' + mail + '&pwd=' + pwd
+        gateway_login_url = 'http://localhost:50052/api/login?usr=' + mail + '&pwd=' + pwd
         response = requests.get(gateway_login_url).json()
         if response['message'] == 'Login successful':
             session['logged'] = True
@@ -42,7 +41,7 @@ def logout():
     return render_template("logout.html")
 
 
-@app.route('/signUp', methods=['POST'])
+@app.route('/sign-up', methods=['POST'])
 def signUp():
     name = request.form.get('txtName')
     surname = request.form.get('txtSurname')
@@ -56,7 +55,7 @@ def signUp():
         flash('Signup failed some required fields are empty')
         return render_template("signUp.html")
     else:
-        gateway_sign_up_url = 'http://localhost:50052/signUp?name=' + name + '&surname=' + surname + '&usr=' + mail + '&pwd=' + pwd
+        gateway_sign_up_url = 'http://localhost:50052/api/sign-up?name=' + name + '&surname=' + surname + '&usr=' + mail + '&pwd=' + pwd
         response = requests.get(gateway_sign_up_url).json()
         if response['message'] == 'Sign up successful':
             session['logged'] = True
@@ -68,6 +67,26 @@ def signUp():
             flash('Signup failed')
             return render_template("signUpFailed.html")
 
+@app.route('/select-route-from-map', methods=['GET'])
+def request_route():
+    starting_point = request.args.get('starting_point')
+    ending_point = request.args.get('ending_point')
+    date = request.args.get('date')
+    arrival_time = request.args.get('arrival_time')
+    travel_time = request.args.get('travel_time')
+    print(starting_point)
+    print(ending_point)
+    print(date)
+    print(arrival_time)
+    print(travel_time)
+    if starting_point == None or ending_point == '' or date == '' or arrival_time == '' or travel_time == '':
+        return render_template("select_route_from_map.html")
+    else:
+        gateway_request_route_url = 'http://localhost:50052/api/route_from_map?starting_point=' + starting_point + '&ending_point=' + ending_point + '&date=' + date + '&arrival_time=' + arrival_time + '&travel_time=' + travel_time
+        print(gateway_request_route_url)
+        response = requests.get(gateway_request_route_url).json()
+        session['response'] = response
+        return render_template("select_route_from_map.html", response=response) 
 
 if __name__ == '__main__':
     app.run(debug=True)
--- gatewayApiPack/gatewayApi/GatewayApi.py
+++ gatewayApiPack/gatewayApi/GatewayApi.py
@@ -1,5 +1,9 @@
 import grpc
 from flask import Flask, json, request
+import xmlrpc.client
+
+import random
+import time
 
 import protos.login_service_cs_pb2
 import protos.login_service_cs_pb2_grpc
@@ -7,7 +11,7 @@ import protos.login_service_cs_pb2_grpc
 api = Flask(__name__)
 
 
-@api.route('/login', methods=['GET'])
+@api.route('/api/login', methods=['GET'])
 def login():
     res = {}
     username = request.args.get('usr')
@@ -23,7 +27,7 @@ def login():
     return json.dumps(res)
 
 
-@api.route('/signUp', methods=['GET'])
+@api.route('/api/sign-up', methods=['GET'])
 def signUp():
     res = {}
     name = request.args.get('name')
@@ -39,6 +43,46 @@ def signUp():
     res['token'] = response.token
     print("LoginService sign up client received: " + response.message + " Token: " + response.token)
     return json.dumps(res)
+
+@api.route('/api/request-route', methods=['GET'])
+def request_route():
+    starting_point = request.args.get('starting_point')
+    ending_point = request.args.get('ending_point')
+
+    dist_matrix = generate_dist_matrix(20, 10)
+    pred_hash = {"0":4, "1":3, "7":8, "3":6, "4":11, "5":14, "0":9, "6":9, "8":9, "11":15, "11":14, "18":11, "13":12, "14":17, "15":19, "16":18, "17":19, "18":19}
+    route = [0, 2, 1, 3, 6, 5, 4, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 10]
+    with xmlrpc.client.ServerProxy("http://server:8000/") as proxy:
+        result = proxy.two_opt_multistart(route, dist_matrix, pred_hash, 10)
+        return json.dumps(result)
+
+@api.route('/api/route-from-map', methods=['GET'])
+def route_from_map():
+    user = request.args.get('user')
+    starting_point = request.args.get('starting_point')
+    ending_point = request.args.get('ending_point')
+    date = request.args.get('date')
+    arrival_time = request.args.get('arrival_time')
+    travel_time = request.args.get('travel_time')
+    print(user, starting_point, ending_point, date, arrival_time, travel_time)
+    with xmlrpc.client.ServerProxy("http://booking_service:8000/") as proxy:
+        result = proxy.insert_booking(user, starting_point, ending_point, date, arrival_time, travel_time)
+        return json.dumps(result)
+        
+        
+def generate_dist_matrix(size, max_val):
+    random.seed(time.time())
+    dist_matrix = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            if i == j:
+                row.append(0)
+            else:
+                val = random.randint(1, max_val)
+                row.append(val)
+        dist_matrix.append(row)
+    return dist_matrix
 
 
 if __name__ == '__main__':
