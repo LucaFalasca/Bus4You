cd7c792924393358827ffad9d7d0f98622cd737f
Matteo Conti <56634851+matteo-conti-97@users.noreply.github.com>
2023-06-02 19:32:57 +0200

    #65 - Bug stored procedure call from login service failing

--- login-service/loginService/dao/user_db/UserDbDao.py
+++ login-service/loginService/dao/user_db/UserDbDao.py
@@ -7,7 +7,7 @@ class UserDbDao:
         self.db_ip = 'mysqlDb' # For docker if the db is deployed in a container
         #self.db_ip = 'host.docker.internal'  # For docker if the db is deployed in the docker container host
         #self.db_ip = 'localhost'
-        self.db_name = 'b4y_userdb'
+        self.db_name = 'b4y_user_db'
         self.usr = 'root'
         self.pwd = 'root'
 
@@ -28,31 +28,35 @@ class UserDbDao:
 
     @staticmethod
     def login_query(conn, mail, password):
+        print("Sono nella funzione login_query della dao")
+        res = None
         if conn is not None:
             print("Connection with db successful")
             curs = conn.cursor()
-            query = "SELECT * FROM user WHERE mail = %s AND pwd = %s"
             args = (mail, password)
-            curs.execute(query, args)
-            res = curs.fetchall()
+            curs.callproc('login', args)
+            conn.commit()
+            for result in curs.stored_results():
+                res = result.fetchall()
             curs.close()
             # Check result
             if res[0][0] == 1:
+                print("Login successful")
                 return 0
             else:
+                print("Login failed")
                 return 1
         else:
             print("Connection with db failed")
             return -1
 
     @staticmethod
-    def sign_up(conn, name, surname, mail, password):
+    def sign_up(conn, name, surname, mail, password, birthdate, username): #Birthdate in format YYYY-MM-DD
         if conn is not None:
             print("Connection with db successful")
             curs = conn.cursor()
-            query = "INSERT INTO user(mail, pwd, username, name, surname) VALUES (%s,%s,%s,%s,%s)"
-            args = (mail, password, mail.split('@')[0], name, surname)
-            curs.execute(query, args)
+            args = (name, surname, mail, password, username, birthdate)
+            curs.callproc('sign_up', args)
             conn.commit()
             curs.close()
             return 0
