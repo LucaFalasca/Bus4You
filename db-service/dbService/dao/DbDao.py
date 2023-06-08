054618774bc4cc9c05d56e8bb60732a1e611a5af
Matteo Conti <56634851+matteo-conti-97@users.noreply.github.com>
2023-06-09 00:19:48 +0200

    #71 - Create a service with the role of mediator between other services and the mysql container

---
+++ db-service/dbService/dao/DbDao.py
@@ -0,0 +1,71 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+class DbDao:
+    def __init__(self):
+        self.db_ip = 'mysqlDb'  # For docker if the db is deployed in a container
+        #self.db_ip = 'host.docker.internal'  # For docker if the db is deployed in the docker container host
+        #self.db_ip = 'localhost'
+        self.db_name = 'b4y_user_db'
+        self.usr = 'root'
+        self.pwd = 'root'
+
+    def connect(self):
+        try:
+            connection = mysql.connector.connect(host=self.db_ip,
+                                                 database=self.db_name,
+                                                 user=self.usr,
+                                                 password=self.pwd)
+            if connection.is_connected():
+                return connection
+            else:
+                print("Error while connecting to MySQL")
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+            return None
+
+    @staticmethod
+    def stop_query(conn):
+        ret = []
+        res = None
+        if conn is not None:
+            print("Connection with db successful")
+            curs = conn.cursor()
+            curs.callproc('get_stops')
+            conn.commit()
+            for result in curs.stored_results():
+                res = result.fetchall()
+            curs.close()
+            for elem in res:
+                ret.append([elem[0], elem[1], elem[2]])
+            print(ret)
+        else:
+            print("Connection with db failed")
+            return -1
+
+    @staticmethod
+    def user_routes_query(conn, usr):
+        ret = []
+        res = None
+        if conn is not None:
+            print("Connection with db successful")
+            curs = conn.cursor()
+            curs.callproc('get_user_routes', (usr,))
+            conn.commit()
+            for result in curs.stored_results():
+                res = result.fetchall()
+            curs.close()
+            print(len(res))
+            for elem in res:
+                '''
+                costo, orario partenza proposto, orario arrivo proposto, stato itinerario proposto, flag percorso passato,
+                stato percorso, scadenza, nome fermata partenza, nome fermata arrivo
+                '''
+                ret.append([elem[0], elem[1], elem[2], elem[3], elem[4], elem[5], elem[6], elem[7], elem[8]])
+            print(ret)
+            return ret
+        else:
+            print("Connection with db failed")
+            return -1
---
+++ db-service/dbService/dbService.py
@@ -0,0 +1,31 @@
+import xmlrpc.server
+from dao.DbDao import DbDao
+
+
+def serve():
+    server = xmlrpc.server.SimpleXMLRPCServer(('', 8000))
+    print("Listening on port 8000...")
+
+    server.register_function(get_stops, "get_stops")
+    server.register_function(get_user_routes, "get_user_routes")
+    server.serve_forever()
+
+
+def get_stops():
+    usr_db = DbDao()
+    conn = usr_db.connect()
+    stop_list = usr_db.stop_query(conn)
+    conn.close()
+    return {"stops": stop_list}
+
+
+def get_user_routes(mail):
+    usr_db = DbDao()
+    conn = usr_db.connect()
+    routes_list = usr_db.user_routes_query(conn, mail)
+    conn.close()
+    return {"routes": routes_list}
+
+
+if __name__ == "__main__":
+    serve()
