415ba6379e6a48377a4f2c501834f1c10b6c671a
LucaFalasca <lucafalasca08@gmail.com>
2023-07-01 21:37:18 +0200

    Prima richiesta al container di ORS funzionante

--- node-selector-service/NodeSelectorService.py
+++ node-selector-service/NodeSelectorService.py
@@ -1,2 +1,7 @@
+import requests
+
+gateway_login_url = 'http://ors-app:8080/ors/v2/directions/driving-car?start=12.527504,41.837339&end=12.611618,41.853708'
+response = requests.get(gateway_login_url).json()
+print(response)
 
 
